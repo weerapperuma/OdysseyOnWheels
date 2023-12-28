6e6d02904a54425c699fb74b162eb8bc943d27d0
weerapperuma <weerapperumasahiru@gmail.com>
2023-12-28 23:18:58 +0530

    Employee table buttons done

--- src/main/java/lk/penguin/rentalWheelzAI/controller/EmployeeManageFormController.java
+++ src/main/java/lk/penguin/rentalWheelzAI/controller/EmployeeManageFormController.java
@@ -1,17 +1,11 @@
 package lk.penguin.rentalWheelzAI.controller;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import lk.penguin.rentalWheelzAI.bo.EmployeeBOImpl;
-import lk.penguin.rentalWheelzAI.dao.impl.EmployeeDAOImpl;
 import lk.penguin.rentalWheelzAI.dto.EmployeeDTO;
 import lk.penguin.rentalWheelzAI.util.SQLUtil;
 
@@ -49,16 +43,7 @@ public class EmployeeManageFormController {
         for(EmployeeDTO dto:dtos){
             createEmployeeRawLoadPane(dto);
         }
-//        if (!dtos.isEmpty()){
-//            employeeidFxId.setText(dtos.get(0).getEmployeeId());
-//            employeeNameFxId.setText(dtos.get(0).getEmployeeName());
-//            employeeEmailFxId.setText(dtos.get(0).getEmpEmail());
-//            employeeNicFxId.setText(dtos.get(0).getEmpNIC());
-//            employeePositionFxId.setText(dtos.get(0).getEmpPosition());
-//            employeeAddressFxId.setText(dtos.get(0).getEmpAddress());
-//            employeeContactFxId.setText(dtos.get(0).getEmpContact());
-//
-//        }
+
     }
 
     private void createEmployeeRawLoadPane(EmployeeDTO dto) throws IOException {
@@ -67,12 +52,6 @@ public class EmployeeManageFormController {
         EmployeeRawFormController controller=loader.getController();
 
         controller.setData(dto);
-//        controller.employeeNameFxId.setText(dto.getEmployeeName());
-//        controller.employeeEmailFxId.setText(dto.getEmpEmail());
-//        controller.employeeNicFxId.setText(dto.getEmpNIC());
-//        controller.employeePositionFxId.setText(dto.getEmpPosition());
-//        controller.employeeAddressFxId.setText(dto.getEmpAddress());
-//        controller.employeeContactFxId.setText(dto.getEmpContact());
 
         mainContainer.getChildren().add(root);
     }
--- src/main/java/lk/penguin/rentalWheelzAI/controller/EmployeeRawFormController.java
+++ src/main/java/lk/penguin/rentalWheelzAI/controller/EmployeeRawFormController.java
@@ -5,9 +5,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import lk.penguin.rentalWheelzAI.dto.EmployeeDTO;
+import lk.penguin.rentalWheelzAI.util.SQLUtil;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.sql.SQLException;
 
 
 public class EmployeeRawFormController {
@@ -37,8 +40,9 @@ public class EmployeeRawFormController {
     private Label lblEmpPosition;
 
     @FXML
-    void btnEmpDeleteOnAction(ActionEvent event) {
-
+    void btnEmpDeleteOnAction(ActionEvent event) throws SQLException, ClassNotFoundException {
+        String empId=lblEmpId.getText();
+        SQLUtil.execute("DELETE FROM employee WHERE employeeId=?",empId);
     }
 
     @FXML
