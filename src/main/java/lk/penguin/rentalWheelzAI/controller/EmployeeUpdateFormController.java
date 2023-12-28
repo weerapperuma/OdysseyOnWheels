9666a2b43bea89c7ce045ba485ad3b9464a85cbf
weerapperuma <weerapperumasahiru@gmail.com>
2023-12-29 01:05:49 +0530

    Employee update form

---
+++ src/main/java/lk/penguin/rentalWheelzAI/controller/EmployeeUpdateFormController.java
@@ -0,0 +1,36 @@
+package lk.penguin.rentalWheelzAI.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class EmployeeUpdateFormController {
+
+    @FXML
+    private Label lblEmpId;
+
+    @FXML
+    private TextField txtEmpAddress;
+
+    @FXML
+    private TextField txtEmpContact;
+
+    @FXML
+    private TextField txtEmpEmail;
+
+    @FXML
+    private TextField txtEmpINic;
+
+    @FXML
+    private TextField txtEmpName;
+
+    @FXML
+    private TextField txtEmpPosition;
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+}
