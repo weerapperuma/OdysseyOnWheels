5ba144cf1752003463e129987d8ffb702f5ced81
weerapperuma <weerapperumasahiru@gmail.com>
2024-01-01 23:04:43 +0530

    main rental entity

---
+++ src/main/java/lk/penguin/OdysseyOnWheels/entity/Language.java
@@ -0,0 +1,18 @@
+package lk.penguin.OdysseyOnWheels.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Language {
+    private int wordSetNb;
+    private String russian;
+    private String sinhala;
+    private String german;
+    private String hindi;
+    private String french;
+    private String english;
+}
