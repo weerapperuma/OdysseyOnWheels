4e62dc6c1502c1b0ba0de55a17462f735416e13d
weerapperuma <weerapperumasahiru@gmail.com>
2024-01-07 18:29:33 +0530

    made transaction util to layerd project easy

---
+++ src/main/java/lk/penguin/OdysseyOnWheels/util/TransactionUtil.java
@@ -0,0 +1,33 @@
+package lk.penguin.OdysseyOnWheels.util;
+
+import lk.penguin.OdysseyOnWheels.db.DbConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class TransactionUtil {
+
+    static Connection connection;
+
+    static {
+        try {
+            connection = DbConnection.getInstance().getConnection();
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public TransactionUtil() throws SQLException,ClassNotFoundException{
+
+    }
+
+    public static void startTransaction() throws SQLException {
+        connection.setAutoCommit(false);
+    }
+    public static void endTransaction() throws SQLException {
+        connection.setAutoCommit(false);
+    }
+    public static void rollBack() throws SQLException {
+        connection.rollback();
+        connection.setAutoCommit(true);
+    }
+}
