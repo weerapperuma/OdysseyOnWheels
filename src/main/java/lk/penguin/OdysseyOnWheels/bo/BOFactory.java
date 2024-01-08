75634f89061f560723c465ea04457c2215f0c520
weerapperuma <weerapperumasahiru@gmail.com>
2024-01-09 00:26:15 +0530

    BOFactory completed

---
+++ src/main/java/lk/penguin/OdysseyOnWheels/bo/BOFactory.java
@@ -0,0 +1,32 @@
+package lk.penguin.OdysseyOnWheels.bo;
+
+import lk.penguin.OdysseyOnWheels.bo.custom.impl.*;
+
+public class BOFactory {
+    private static BOFactory boFactory;
+
+    private BOFactory(){}
+
+    public static BOFactory getBoFactory(){
+        return (boFactory==null)? boFactory=new BOFactory():boFactory;
+    }
+    public enum BOType{
+        WELCOME,CUSTOMER,EMPLOYEE,TRANSACTION,RENT,VEHICLE
+    }
+    public SuperBO getBO(BOType boType){
+        switch (boType){
+            case WELCOME:
+                return new WelcomeBoImpl();
+            case CUSTOMER:
+                return new CustomerBOImpl();
+            case RENT:
+                return new RentBOImpl();
+            case VEHICLE:
+                return new VehicleBOImpl();
+            case EMPLOYEE:
+                //return new EmployeeBOImpl();
+            default:
+                return null;
+        }
+    }
+}
