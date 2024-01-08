189d0a7f3af3d2d56778e0c988d5cc7b9875bea9
weerapperuma <weerapperumasahiru@gmail.com>
2024-01-09 02:50:11 +0530

    login form maintained

--- src/main/java/lk/penguin/OdysseyOnWheels/controller/LoginFormController.java
+++ src/main/java/lk/penguin/OdysseyOnWheels/controller/LoginFormController.java
@@ -1,52 +1,25 @@
+
 package lk.penguin.OdysseyOnWheels.controller;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import lk.penguin.OdysseyOnWheels.bo.custom.UserBO;
-import lk.penguin.OdysseyOnWheels.bo.custom.impl.UserBOImpl;
-import lk.penguin.OdysseyOnWheels.util.Navigation;
-
-import java.io.IOException;
-import java.sql.SQLException;
 
 public class LoginFormController {
-    @FXML
-    private PasswordField txtPassword;
 
     @FXML
-    private TextField txtUserName;
-    @FXML
-    private Button btnLoginfxId;
+    private JFXButton btnpassworduser;
 
-    UserBO userBO=new UserBOImpl();
     @FXML
-    void btnLoginOnAction(ActionEvent event) throws IOException, SQLException, ClassNotFoundException {
-        if(userBO.checkCredentials(txtUserName.getText(),txtPassword.getText())){
-            Navigation.closePopup();
-            Navigation.closeCurrentWindow(event);
-            Navigation.switchNavigation( "backgroundForm.fxml",event);
+    private PasswordField txtPasswordUser;
 
-        }
-        else {
-            new Alert(Alert.AlertType.ERROR,"Invalid Login Details");
-        }
+    @FXML
+    void btnUserPasswordOnAction(ActionEvent event) {
 
     }
-    @FXML
-    void backOnAction(ActionEvent event) throws IOException {
-        Navigation.switchNavigation("welcomeForm.fxml",event);
-    }
 
-    @FXML
-    void hplinkTroubleLogin(ActionEvent event) {
-
-    }
     public void initialize(){
-        txtUserName.setOnAction(event ->txtPassword.requestFocus());
-        txtPassword.setOnAction(event ->btnLoginfxId.fire());
+        txtPasswordUser.setOnAction(event ->btnpassworduser.requestFocus());
     }
 }
